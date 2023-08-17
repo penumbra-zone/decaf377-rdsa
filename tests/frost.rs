e3b052f2374be94cd958daa2f4fdc48744c63f76
Henry de Valence <hdevalence@penumbralabs.xyz>
2023-08-16 20:54:46 -0700

    frost: add randomization support

--- tests/frost.rs
+++ tests/frost.rs
@@ -1,11 +1,10 @@
 use std::collections::{BTreeMap, HashMap};
 
-use decaf377_rdsa::frost::keys::dkg::round1;
 use rand::thread_rng;
 
-use decaf377_rdsa::*;
-
+use ark_ff::UniformRand;
 use decaf377_rdsa::frost::Identifier;
+use decaf377_rdsa::*;
 
 #[test]
 fn simple_dkg_and_signing_flow() -> anyhow::Result<()> {
@@ -28,10 +27,7 @@ fn simple_dkg_and_signing_flow() -> anyhow::Result<()> {
         round1_packages.insert(*id, package);
     }
 
-    dbg!(&round1_secrets);
-    dbg!(&round1_packages);
     // Round 1 is a broadcast, so it's enough to copy all the round 1 packages.
-
     let mut round2_secrets = HashMap::new();
     let mut round2_packages = HashMap::new();
 
@@ -41,23 +37,18 @@ fn simple_dkg_and_signing_flow() -> anyhow::Result<()> {
         let mut round1_packages_except_us = round1_packages.clone();
         round1_packages_except_us.remove(id);
 
-        //let (secret, packages) = frost::keys::dkg::part2(round1_secret, &round1_packages)?;
         let (secret, packages) =
             frost::keys::dkg::part2(round1_secret, &round1_packages_except_us)?;
         round2_secrets.insert(*id, secret);
         round2_packages.insert(*id, packages);
     }
 
-    dbg!(&round2_secrets);
-    dbg!(&round2_packages);
-
-    // Round 2 is point-to-point (and encrypted! but we're faking it), so we need to
+    // Round 2 is point-to-point (but we're faking it), so we need to
     // build a map of messages received by each participant.
 
     let mut shares = HashMap::new();
     let mut public_key_packages = HashMap::new();
 
-    dbg!("got here");
     for id in &ids {
         let mut recvd_packages = HashMap::new();
         for (other_id, its_packages) in &round2_packages {
@@ -66,24 +57,17 @@ fn simple_dkg_and_signing_flow() -> anyhow::Result<()> {
             }
             recvd_packages.insert(*other_id, its_packages.get(id).unwrap().clone());
         }
-        dbg!(&id);
-        dbg!(&recvd_packages);
 
         let mut round1_packages_except_us = round1_packages.clone();
         round1_packages_except_us.remove(id);
 
         let round2_secret = round2_secrets.remove(id).unwrap();
         let (key_package, public_key_package) =
-            //frost::keys::dkg::part3(&round2_secret, &round1_packages, &recvd_packages)?;
             frost::keys::dkg::part3(&round2_secret, &round1_packages_except_us, &recvd_packages)?;
 
         shares.insert(id, key_package);
         public_key_packages.insert(*id, public_key_package);
     }
-    dbg!("got here");
-
-    dbg!(&shares);
-    dbg!(&public_key_packages);
 
     // Now try signing.
     const MSG: &[u8] = b"hello world";
@@ -98,9 +82,6 @@ fn simple_dkg_and_signing_flow() -> anyhow::Result<()> {
         signing_commitments.insert(*id, commitment);
         sign_round1_nonces.insert(*id, nonce);
     }
-
-    dbg!(&signing_commitments);
-    //dbg!(&sign_round1_nonces);
 
     let signing_package = frost::SigningPackage::new(signing_commitments, MSG);
 
@@ -136,6 +117,51 @@ fn simple_dkg_and_signing_flow() -> anyhow::Result<()> {
 
     // Verify the signature
     vk.verify(MSG, &signature)?;
+
+    // Now try randomized signing.
+
+    let mut signing_commitments = BTreeMap::new();
+    let mut sign_round1_nonces = HashMap::new();
+
+    for id in &ids {
+        let (nonce, commitment) =
+            frost::round1::commit(&shares[id].secret_share(), &mut thread_rng());
+        signing_commitments.insert(*id, commitment);
+        sign_round1_nonces.insert(*id, nonce);
+    }
+
+    let r = Fr::rand(&mut thread_rng());
+
+    let signing_package = frost::SigningPackage::new(signing_commitments, MSG);
+
+    let mut sign_round2_signature_shares = HashMap::new();
+
+    for id in &ids {
+        let share = frost::round2::sign_randomized(
+            &signing_package,
+            &sign_round1_nonces.get(id).unwrap(),
+            &shares.get(id).unwrap(),
+            r.clone(),
+        )?;
+        sign_round2_signature_shares.insert(*id, share);
+    }
+
+    // Aggregate the signature shares
+
+    let signature_bytes = frost::aggregate_randomized(
+        &signing_package,
+        &sign_round2_signature_shares,
+        public_key_packages.values().next().unwrap(),
+        r.clone(),
+    )?
+    .serialize();
+
+    let signature = Signature::<SpendAuth>::from(signature_bytes);
+
+    // Use r to randomize the verification key independently of FROST code
+    let r_vk = vk.randomize(&r);
+    // ... and verify with the (externally) randomized key
+    r_vk.verify(MSG, &signature)?;
 
     Ok(())
 }
